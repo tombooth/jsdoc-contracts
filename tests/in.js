63594bb4f51e49e583c5b43dcc6f87c885239426
Tom Booth <tomhbooth@gmail.com>
2012-03-22 16:49:34 +0000

    Initial commit

---
+++ tests/in.js
@@ -0,0 +1,29 @@
+
+(function() {
+
+   /* a comment*/
+   function inc(i) {
+      /* comment */
+      return i+1;
+   }
+   /* blah */
+
+
+   /**
+    * JSDoc comment
+    * @param {Number} i number to decrement
+    * @param {Number} [n] number to decriment by, defauls to 1
+    * @returns {Number} decrimented number
+    */
+   inc.prototype.dec = function dec(i, n) {
+      n = n || 1;
+      return i-n;
+   }
+
+   /* blah blah*/
+   console.log(inc(2));
+   console.log(inc.prototype.dec('asdf'));
+
+})();
+
+
